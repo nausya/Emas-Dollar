217bfd0d565be538242b040aff5fba3204966a40
Dwi Andriyanto <61266982+nausya@users.noreply.github.com>
2024-11-09 12:15:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,63 +28,19 @@ from sklearn.metrics import r2_score, mean_absolute_error
 import os
 pd.set_option('future.no_silent_downcasting', True)
 
-# Show the page title and description.
-st.set_page_config(page_title="Grafik Emas Terhadap Dollar", page_icon="🎬")
-st.title("🎬 eMAS DOllar")
-st.write(
-    """
-    Grafik Perdangan Emas Terhadap Dollar Amerika
-    """
-)
+######Halaman Utama
+st.set_page_config(page_title="DOLMAS", layout="wide")
+st.header('TRADING EMAS TERHADAP DOLLAR')
+######End of Halaman Utama
 
+###### FUNGSI MENU #############
+def main():
+### Taruh kalo ada menu
+###### END OF FUNGSI MENU #############
+@st.cache_resource
 
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
-def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
+####### FUNGSI AMBIL DATA SAHAM ############
+def download_data(op, start_date, end_date):
+    df = yf.download(op, start=start_date, end=end_date, progress=False)
     return df
-
-
-df = load_data()
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
-
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
-
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
-
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
+####### END OF FUNGSI AMBIL DATA SAHAM ############
